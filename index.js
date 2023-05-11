0df75ed72b0adec34c808e407106cd2834391c0e
Matthew Denune <mattdenune@gmail.com>
2023-05-11 15:13:07 -0400

    building out routes

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+const app = new express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+});
+
+app.get('/post', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
+app.listen(4000, () => {
+  console.log('App listening on port 4000')
+});
